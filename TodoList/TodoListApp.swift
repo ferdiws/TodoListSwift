c2f7fa087624230f76423302539034c30b1cde4a
Ferdinand <swferdi@gmail.com>
2023-09-28 21:14:42 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ferdinand Wijaya Susilo on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
